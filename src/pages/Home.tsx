4e1d62426489fe49da21eec01c9457a5cfec1483
Tony <gogo6570@gmail.com>
2024-02-25 13:12:31 +0900

    feat: 레이아웃 기본적인 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import Footer from '../components/common/Footer';
+import Header from '../components/common/Header';
+
+function Home() {
+    return (
+        <>
+            <Header></Header>
+            <div>Home body</div>
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default Home;
