aaac4855353ccc92500e3c76d2147ec5e4dbd70c
Tony <gogo6570@gmail.com>
2024-02-26 18:19:44 +0900

    feat : 로컬 스토리지를 활용해 테마 상태 저장 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import { GlobalStyle } from './style/global';
+import { ThemeContext, BookStoreThemeProvider } from './context/themeContext';
+import { ThemeName, getTheme } from './style/theme';
+import ThemeSwitcher from './components/header/ThemeSwitcher';
+import { useContext, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { dark, light } from './style/theme';
 
 function App() {
     // return <Layout children={<Home />} />;
+
     return (
-        <ThemeProvider theme={light}>
-            <GlobalStyle themeName="light" />
+        <BookStoreThemeProvider>
+            <ThemeSwitcher />
             <Layout>
                 <Home /> {/* children  이렇게도 가능 */}
             </Layout>
-        </ThemeProvider>
+        </BookStoreThemeProvider>
     );
 }
 
