1207d23879442aa4ab35dfee8e60f30e71192754
Tony <gogo6570@gmail.com>
2024-02-28 03:28:53 +0900

    feat: 라우트 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
-import { GlobalStyle } from './style/global';
-import { ThemeContext, BookStoreThemeProvider } from './context/themeContext';
-import { ThemeName, getTheme } from './style/theme';
-import ThemeSwitcher from './components/header/ThemeSwitcher';
-import { useContext, useState } from 'react';
-import { ThemeProvider } from 'styled-components';
+import { BookStoreThemeProvider } from './context/themeContext';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Error from './components/common/Error';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: (
+            <Layout>
+                <Home />
+            </Layout>
+        ),
+        errorElement: <Error />, //하위에 모두 적용됨
+    },
+    {
+        path: '/books',
+        element: (
+            <Layout>
+                <div>도서 목록</div>
+            </Layout>
+        ),
+    },
+]);
 
 function App() {
     // return <Layout children={<Home />} />;
 
     return (
         <BookStoreThemeProvider>
-            <ThemeSwitcher />
-            <Layout>
-                <Home /> {/* children  이렇게도 가능 */}
-            </Layout>
+            <RouterProvider router={router} />
         </BookStoreThemeProvider>
     );
 }
